903a3b9bdec51feb7ad29f12a8c63a3fddef6a50
Jan Kaifer <jan@kaifer.cz>
2021-10-17 16:24:09 +0200

    added handles for rotation

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import p5Types from "p5";
 // import nj from "numjs";
 
 import "./App.css";
-import { Slider } from "@mui/material";
+import { Slider, Stack } from "@mui/material";
 
 type Point = [number, number, number];
 // type Vector = nj.NdArray<number>;
@@ -17,15 +17,23 @@ type Point = [number, number, number];
 // const v2a = (p: nj.NdArray) => [p.get(0), p.get(1), p.get(2)] as const;
 // const a2v = (arr: [number, number, number]) => nj.array(arr);
 
-const Canvas = ({ tRef }: { tRef: React.MutableRefObject<number> }) => {
+const Canvas = ({
+  t,
+  x,
+  y,
+  z,
+}: {
+  t: number;
+  x: number;
+  y: number;
+  z: number;
+}) => {
   const setup = (p5: p5Types, canvasParentRef: Element) => {
     p5.createCanvas(300, 500, p5.WEBGL).parent(canvasParentRef);
     p5.colorMode(p5.RGB);
   };
 
   const draw = (p5: p5Types) => {
-    const t = tRef.current;
-
     const unit = 100;
     const l0 = 0.2 * unit;
     const q1 = 0.5 * t;
@@ -73,8 +81,9 @@ const Canvas = ({ tRef }: { tRef: React.MutableRefObject<number> }) => {
     };
 
     p5.background(255);
-    p5.rotateX(p5.TWO_PI * 0.2);
-    p5.rotateZ(p5.TWO_PI * 0.3);
+    p5.rotateZ(p5.TWO_PI * z);
+    p5.rotateX(p5.TWO_PI * x);
+    p5.rotateY(p5.TWO_PI * y);
     p5.plane(200);
 
     drawPoint();
@@ -87,22 +96,49 @@ const Canvas = ({ tRef }: { tRef: React.MutableRefObject<number> }) => {
   return <Sketch setup={setup} draw={draw} />;
 };
 
+const MySlide = ({ val, setVal, min, max, step, name }: any) => {
+  return (
+    <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+      <div
+        style={{
+          paddingLeft: 20,
+          paddingRight: 20,
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <span style={{ paddingRight: 20 }}>
+          {name}={val}
+        </span>
+        <Slider
+          style={{ width: 100 }}
+          value={val}
+          onChange={(_: any, newVal: any) => setVal(newVal)}
+          step={step}
+          min={min}
+          max={max}
+        />
+      </div>
+    </Stack>
+  );
+};
+
 const App = () => {
   const [t, setT] = useState(0);
+  const [z, setZ] = useState(0);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
 
   return (
     <div className="App">
-      <div style={{ padding: 40 }}>
-        <Slider
-          value={t}
-          onChange={(e, newVal) => setT(newVal as number)}
-          step={0.01}
-          min={0}
-          max={3}
-        />
-        t={t}
-      </div>
-      <Canvas tRef={{ current: t }} />
+      <MySlide val={t} setVal={setT} min={0} max={3} step={0.01} name="t" />
+      <MySlide val={z} setVal={setZ} min={0} max={1} step={0.01} name="z" />
+      <MySlide val={x} setVal={setX} min={0} max={1} step={0.01} name="x" />
+      <MySlide val={y} setVal={setY} min={0} max={1} step={0.01} name="y" />
+      <Canvas t={t} z={z} x={x} y={y} />
     </div>
   );
 };
